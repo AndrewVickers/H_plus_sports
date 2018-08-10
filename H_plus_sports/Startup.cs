d4c6231eac0c4e30ca857e9aecfc61eb9a14d3f4
CAMPUS\nav11 <andrew.vickers@ncl.ac.uk>
2018-08-10 15:30:55 +0100

    Routing

--- H_plus_sports/Startup.cs
+++ H_plus_sports/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using H_plus_sports.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,9 @@ namespace H_plus_sports
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connectionString =
+                "Server=tcp:avhssports.database.windows.net,1433;Initial Catalog=H_Plus_Sports;Persist Security Info=False;User ID=andrewvickers;Password=1515UT&B0bby65Da;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<H_Plus_SportsContext>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
